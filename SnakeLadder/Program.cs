eb8d1e17ed3da31bdbd42a8f60679d185e3b88d4
Ankit <ankitcool4370@gmail.com>
2022-12-27 20:20:39 +0530

    Welcome to the Snake and Ladder

---
+++ SnakeLadder/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+namespace SnakeLadder
+{
+    internal class SnakeLadder
+    {
+        public static void WelcomeMsg()
+        {
+            Console.WriteLine("Welcome to Snake and Ladder Game");
+        }
+        static void Main(string[] args)
+        {
+            WelcomeMsg();
+            int position = 0;
+            Console.WriteLine($"Initial starting position of Player :{position}");
+            Console.ReadLine();
+        }
+    }
+}
